95ab60c7cb91f284baa66c83e65df38a5c619a11
Jordan Defay <jordan.defay@hotmail.com>
2024-02-24 20:45:08 +0100

    Add CSS on navbar and the responsive menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,41 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './navbar.css';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
-
+import { RiMenu3Line, RiCloseLin, RiCloseLine } from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='chamalhat__navbar'>
       <div className='chamalhat__navbar-links'>
         <div className='chamalhat__navbar-links_logo'>
           <img src={logo} alt="logo" />
         </div>
+        <div className = 'chamalhat__navbar-links_container'>
+          <p><a href="#home">Accueil</a></p>
+          <p><a href="#possibility">Portfolio</a></p>
+          <p><a href="#wchamalhat">A propos</a></p>
+          <p><a href="#features">Contactez-nous</a></p>
+          <p><a href="#blog">Blog</a></p>
+        </div>
+      </div>
+      <div className='chamalhat__navbar-sign'>
+        <p>Se connecter</p>
+        <button type= "button">S'inscrire</button>
+      </div>
+      <div className='chamalhat__navbar-menu'>
+        {toggleMenu
+        ? <RiCloseLine color= "#fff" size={27} onClick={() => setToggleMenu(false)} />
+        : <RiMenu3Line color= "#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className='chamalhat__navbar-menu_container scale-up-center'>
+            <div className='chamalhat__navbar-menu_container-links'>
+              
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )

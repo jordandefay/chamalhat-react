e7203b24ba6d02d31dcfab94d422061683a3bff2
Jordan Defay <jordan.defay@hotmail.com>
2024-02-24 16:57:15 +0100

    Creation of 'containers'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { Footer, Blog, Possibility, Features, whatChamalHat, Header } from './containers'; 
+
 const App = () => {
   return (
     <div>
---
+++ src/containers/whatChamalHat/whatChamalHat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatchamalhat.css';
+const whatsChamalHat = () => {
+  return (
+    <div>whatsChamalHat</div>
+  )
+}
+
+export default whatsChamalHat

92604fec79932d712b6cc8d6ba9b43f555a2844a
Jordan Defay <jordan.defay@hotmail.com>
2024-02-28 19:42:21 +0100

    Navbar and Header done, begin Brand

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,17 @@
-import React, {useState} from 'react'
+import React, {useState} from 'react';
 import './navbar.css';
 import { RiMenu3Line, RiCloseLin, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
+
+const Menu = () => (
+  <>
+    <p><a href="#home">Accueil</a></p>
+    <p><a href="#possibility">Portfolio</a></p>
+    <p><a href="#wchamalhat">A propos</a></p>
+    <p><a href="#features">Contactez-nous</a></p>
+    <p><a href="#blog">Blog</a></p>
+  </>
+)
 const Navbar = () => {
 
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -13,11 +23,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className = 'chamalhat__navbar-links_container'>
-          <p><a href="#home">Accueil</a></p>
-          <p><a href="#possibility">Portfolio</a></p>
-          <p><a href="#wchamalhat">A propos</a></p>
-          <p><a href="#features">Contactez-nous</a></p>
-          <p><a href="#blog">Blog</a></p>
+          <Menu />
         </div>
       </div>
       <div className='chamalhat__navbar-sign'>
@@ -32,7 +38,11 @@ const Navbar = () => {
         {toggleMenu && (
           <div className='chamalhat__navbar-menu_container scale-up-center'>
             <div className='chamalhat__navbar-menu_container-links'>
-              
+            <Menu />
+            <div className='chamalhat__navbar-sign'>
+          <p>Se connecter</p>
+          <button type= "button">S'inscrire</button>
+          </div>
             </div>
           </div>
         )}
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="chamalhat__header section__padding" id="home">
+    <div className='chamalhat__header-content'>
+      <h1 className='gradient__text'>
+        Let's build something amazing with ChamalHat
+      </h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       Nibh nisl condimentum id venenatis a condimentum vitae sapien. 
+       Vel pharetra vel turpis nunc eget lorem dolor sed viverra.</p>
+      <div className='chamalhat__header-content__input'>
+        <input type="email" placeholder='Your e-mail adress' />
+        <button type="button">Get Started</button>
+      </div>
+      <div className='chamalhat__header-content__people'>
+        <img src={people} alt="people"/>
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+    <div className='chamalhat__header-image'>
+      <img src={ai} alt="ai"/>
+      </div>
+    </div>
   )
 }
 
